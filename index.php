1c53988aeda3c2bbf0c09a6ac646046a88ba4d86
Ayu Delima <Ayu Delima>
2016-01-17 17:38:52 +0700

    nambahain navbar

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
   <head>
     <meta charset="utf-8">
     <title>Latihan Lagi</title>
+
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
   </head>
   <body>
-
+    <main>
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo">Logo</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
+            <li><a href="badges.html"><i class="material-icons right">view_module</i>Link with Right Icon</a></li>
+          </ul>
+        </div>
+      </nav>
+    </main>
+          <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
   </body>
+
 </html>
